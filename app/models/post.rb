4a49c9233e980f189086087e5b5542ad90456842
gbs4ever <gbs4ever2@gmail.com>
2019-03-25 23:38:07 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,26 @@
 class Post < ActiveRecord::Base
-  validates :title, presence: true ,length: { minimum: 2 }
-  validates :title
+  validates :title, presence: true 
+  validates  :content, length: { minimum: 250 }
+  validates  :summary, length: { maximum: 250 }
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate  :clickbait?
+
+
+  ARR=[/Won't Believe/i,
+    /Secret/i,
+    /Top [0-9]*/i,
+    /Guess/i]
+
+
+  def clickbait?
+    if ARR.none? { |pat| pat.match title }
+      errors.add(:title, "must be clickbait")
+    end
+  end
+  
+  
+  
+  
+
+
 end
